073f08dae863fb0048ba4e2f969607b20944670d
MarcAnthonyFanfan <56124184+MarcAnthonyFanfan@users.noreply.github.com>
2020-01-10 10:31:37 -0500

    Change host to mysql

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 // mysql setup
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'mysql',
     port: '3306',
     socketPath: '/var/run/mysqld/mysqld.sock',
     user: 'node',
